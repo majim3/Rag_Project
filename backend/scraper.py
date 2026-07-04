7ae173fd0def0406d4012ed8a3a4ab6a24270d5b
Majim3 <leogong3@gmail.com>
2026-07-04 05:13:46 +0300

    scraping start code got names of the jobs

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,1 +1,24 @@
-test
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://duunitori.fi/tyopaikat?haku=it"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+Jobs = soup.find_all("div", class_="grid grid--middle job-box job-box--lg")
+
+contents = soup.find_all("a", class_="job-box__hover gtm-search-result")
+print(f"Data: {Jobs.__len__()}")
+print(f"Content: {contents.__len__()}")
+
+
+for job in Jobs:
+    content = job.find("div", class_="job-box__content")
+   
+    
+    name = content.find("h3", class_="job-box__title").text.strip()
+    print(f"name: {name}")
+    
+    
+    
